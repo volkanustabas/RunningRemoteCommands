6027e0611baacd1ff3fa98e81c6228d9a7b552c0
Volkan <vlkn0888@gmail.com>
2021-01-28 00:54:41 +0300

    Project started

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace RunningRemoteCommands
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.lbl_info_ipAdress = new System.Windows.Forms.Label();
+            this.tb_ipAdress = new System.Windows.Forms.TextBox();
+            this.lbl_info_command = new System.Windows.Forms.Label();
+            this.tb_command = new System.Windows.Forms.TextBox();
+            this.btn_run = new System.Windows.Forms.Button();
+            this.lbl_info_psExecLocation = new System.Windows.Forms.Label();
+            this.btn_browse = new System.Windows.Forms.Button();
+            this.bw_run = new System.ComponentModel.BackgroundWorker();
+            this.lbl_location = new System.Windows.Forms.Label();
+            this.ofd_location = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // lbl_info_ipAdress
+            // 
+            this.lbl_info_ipAdress.AutoSize = true;
+            this.lbl_info_ipAdress.Location = new System.Drawing.Point(12, 97);
+            this.lbl_info_ipAdress.Name = "lbl_info_ipAdress";
+            this.lbl_info_ipAdress.Size = new System.Drawing.Size(123, 13);
+            this.lbl_info_ipAdress.TabIndex = 0;
+            this.lbl_info_ipAdress.Text = "Remote Computer Name";
+            // 
+            // tb_ipAdress
+            // 
+            this.tb_ipAdress.Location = new System.Drawing.Point(15, 114);
+            this.tb_ipAdress.Name = "tb_ipAdress";
+            this.tb_ipAdress.Size = new System.Drawing.Size(264, 20);
+            this.tb_ipAdress.TabIndex = 1;
+            // 
+            // lbl_info_command
+            // 
+            this.lbl_info_command.AutoSize = true;
+            this.lbl_info_command.Location = new System.Drawing.Point(12, 149);
+            this.lbl_info_command.Name = "lbl_info_command";
+            this.lbl_info_command.Size = new System.Drawing.Size(195, 13);
+            this.lbl_info_command.TabIndex = 2;
+            this.lbl_info_command.Text = "Command (Example: taskmgr, devmgmt)";
+            // 
+            // tb_command
+            // 
+            this.tb_command.Location = new System.Drawing.Point(15, 166);
+            this.tb_command.Name = "tb_command";
+            this.tb_command.Size = new System.Drawing.Size(264, 20);
+            this.tb_command.TabIndex = 3;
+            // 
+            // btn_run
+            // 
+            this.btn_run.Location = new System.Drawing.Point(15, 215);
+            this.btn_run.Name = "btn_run";
+            this.btn_run.Size = new System.Drawing.Size(264, 23);
+            this.btn_run.TabIndex = 4;
+            this.btn_run.Text = "Run";
+            this.btn_run.UseVisualStyleBackColor = true;
+            this.btn_run.Click += new System.EventHandler(this.btn_run_Click);
+            // 
+            // lbl_info_psExecLocation
+            // 
+            this.lbl_info_psExecLocation.AutoSize = true;
+            this.lbl_info_psExecLocation.Location = new System.Drawing.Point(12, 13);
+            this.lbl_info_psExecLocation.Name = "lbl_info_psExecLocation";
+            this.lbl_info_psExecLocation.Size = new System.Drawing.Size(87, 13);
+            this.lbl_info_psExecLocation.TabIndex = 5;
+            this.lbl_info_psExecLocation.Text = "PsExec Location";
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Location = new System.Drawing.Point(15, 30);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(264, 23);
+            this.btn_browse.TabIndex = 6;
+            this.btn_browse.Text = "Browse";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            this.btn_browse.Click += new System.EventHandler(this.btn_browse_Click);
+            // 
+            // bw_run
+            // 
+            this.bw_run.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bw_run_DoWork);
+            this.bw_run.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bw_run_RunWorkerCompleted);
+            // 
+            // lbl_location
+            // 
+            this.lbl_location.AutoSize = true;
+            this.lbl_location.ForeColor = System.Drawing.Color.Red;
+            this.lbl_location.Location = new System.Drawing.Point(15, 60);
+            this.lbl_location.Name = "lbl_location";
+            this.lbl_location.Size = new System.Drawing.Size(0, 13);
+            this.lbl_location.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 265);
+            this.Controls.Add(this.lbl_location);
+            this.Controls.Add(this.btn_browse);
+            this.Controls.Add(this.lbl_info_psExecLocation);
+            this.Controls.Add(this.btn_run);
+            this.Controls.Add(this.tb_command);
+            this.Controls.Add(this.lbl_info_command);
+            this.Controls.Add(this.tb_ipAdress);
+            this.Controls.Add(this.lbl_info_ipAdress);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Running Remote Commands";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_info_ipAdress;
+        private System.Windows.Forms.TextBox tb_ipAdress;
+        private System.Windows.Forms.Label lbl_info_command;
+        private System.Windows.Forms.TextBox tb_command;
+        private System.Windows.Forms.Button btn_run;
+        private System.Windows.Forms.Label lbl_info_psExecLocation;
+        private System.Windows.Forms.Button btn_browse;
+        private System.ComponentModel.BackgroundWorker bw_run;
+        private System.Windows.Forms.Label lbl_location;
+        private System.Windows.Forms.OpenFileDialog ofd_location;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace RunningRemoteCommands
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+
+        private void btn_browse_Click(object sender, EventArgs e)
+        {
+            ofd_location.ShowDialog();
+            ofd_location.InitialDirectory = @"C:\Users\%username%\Downloads";
+            var path = ofd_location.FileName;
+            lbl_location.Text = path;
+        }
+
+        private void btn_run_Click(object sender, EventArgs e)
+        {
+            if (lbl_location.Text == "")
+            {
+                MessageBox.Show(@"can you select psexec location", @"Warning");
+            }
+            else
+            {
+                if (PingHost(tb_ipAdress.Text))
+                {
+
+                    if (tb_command.Text =="")
+                    {
+                        MessageBox.Show(@"Command cannot be empty", @"Warning");
+                    }
+                    else
+                    {
+                        if (bw_run.IsBusy != true)
+                        {
+                            bw_run.RunWorkerAsync();
+                            btn_browse.Enabled = false;
+                            btn_run.Enabled = false;
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(@"Wrong ip adress or computer name", @"Warning");
+                }
+            }
+        }
+
+        private void bw_run_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BeginInvoke((MethodInvoker) delegate { });
+            RunWithPsExec(tb_ipAdress.Text, tb_command.Text, lbl_location.Text);
+        }
+
+
+        private void bw_run_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btn_browse.Enabled = true;
+            btn_run.Enabled = true;
+        }
+
+        public void RunWithPsExec(string ipAdress, string parameter, string psExecLocation)
+        {
+            var usernamePasswordForPsExec = " -u Administrator -p \"password\"";
+            var process = new Process();
+            var locationForPsExec = psExecLocation;
+            var runWithPsExec = @" C:\Windows\System32\cmd.exe ";
+
+            var processInfo = new ProcessStartInfo
+            {
+                UseShellExecute = false,
+                FileName = locationForPsExec,
+                Arguments = locationForPsExec + " -i -d " + "\\\\" + ipAdress + usernamePasswordForPsExec +
+                            runWithPsExec + "/c start " + parameter + ""
+            };
+            process.StartInfo = processInfo;
+            process.Start();
+        }
+
+        public bool PingHost(string remoteComputerIpAdress)
+        {
+            var pingable = false;
+            var pinger = new Ping();
+
+            try
+            {
+                var reply = pinger.Send(remoteComputerIpAdress);
+                if (reply != null) pingable = reply.Status == IPStatus.Success;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            return pingable;
+        }
+    }
+}
